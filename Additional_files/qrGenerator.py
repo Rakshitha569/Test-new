eda143f1a5e6127d1a15a42cac7c8fe00d2a3d36
RAKSHITHA R SHETTY <rakshitha.shetty@mercedes-benz.com>
2024-10-23 08:47:30 +0530

    Delete Additional_files directory

--- Additional_files/qrGenerator.py
+++
@@ -1,7 +0,0 @@
-import demoji
-
-demoji.download_codes()
-
-fruits = ['apple', 'banana', 'cherry', 'date']
-sorted_fruits = sorted(fruits)
-print(sorted_fruits)  # Output: ['apple', 'banana', 'cherry', 'date']
