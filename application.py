74666f4c1554e082fd05acc801442ab10f3fcdd7
RAKSHITHA R SHETTY <rakshitha.shetty@mercedes-benz.com>
2024-11-12 20:36:45 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,20 +1,21 @@
-from flask import Flask
+print "hello"
+# from flask import Flask
 
-from flask import Flask
+# from flask import Flask
 
-application=Flask(__name__)
+# application=Flask(__name__)
 
-@application.route("/")
-def hello_world():
- return 'Hello World'
+# @application.route("/")
+# def hello_world():
+#  return 'Hello World'
 
-''' from flask import jsonify
-app = Flask(__name__)
-@app.route('/numbers/')
+# ''' from flask import jsonify
+# app = Flask(__name__)
+# @app.route('/numbers/')
 
-def print_list():
-    #a=jsonify(list(range(5)))
-    return jsonify(list(range(5))) '''
-#if __name__=='__main__':
+# def print_list():
+#     #a=jsonify(list(range(5)))
+#     return jsonify(list(range(5))) '''
+# #if __name__=='__main__':
  
  
